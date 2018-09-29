376229c7e637af5503e9d7283b1106d5e7ffa6e3
David Hiller <HillerDavid@users.noreply.github.com>
2018-09-29 19:55:31 -0400

    Added more detailed psuedocode. Small tweaks to js code.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,11 @@
 $(document).ready(function () {
     //Starting Array
     var topics = ["labrador", "pug", "dalmation"]
-    
-    //
+
+    //Function to generate buttons from array
     function loadButtons() {
 
+        //Loop to create and add new buttons to page
         for (var i = 0; i < topics.length; i++) {
             var newButton = $("<button>").text(topics[i])
             newButton.addClass("gif-button")
@@ -13,25 +14,38 @@ $(document).ready(function () {
             $("#gif-button-section").append(newButton)
         }
     }
+
+    //Load initial array
     loadButtons()
 
+    //Add user input to array and reload array to add new buttons
     $("#add-gif-button").click(function (event) {
         event.preventDefault()
         userInput = $("#user-input").val().trim()
-        if ($.inArray(userInput, topics) === -1) {
+
+        //Check if user input already exists in array
+        //Alert that button already exists
+        if ($.inArray(userInput.toLowerCase(), topics) !== -1) {
+            alert("Button already exists for " + userInput)
+        }
+
+        //Add user input to array and reload buttons
+        else {
             topics.push(userInput)
             $("#gif-button-section").empty()
             loadButtons()
-        } else {
-            alert("Button already exists for " + userInput)
         }
+
+        //Clears input field
         $("#user-input").val("")
     })
 
+    //Click event listener for buttons created
     $("body").on("click", ".gif-button", function () {
         var searchItem = $(this).attr("data-query")
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchItem + "&api_key=Wir4pR8vBsdXyktmyqIESfIqQZ9SB1aq&limit=10"
 
+        //Call to giphy API to get and display gif images
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -49,9 +63,11 @@ $(document).ready(function () {
 
     })
 
+    //Click event listener for gifs added to page
     $("body").on("click", ".gif", function () {
         var state = $(this).attr("data-state")
 
+        //Toggles gif src between still image and animated image
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"))
             $(this).attr("data-state", "animate")
