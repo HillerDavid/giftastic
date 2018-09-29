a68c7d768435a15070cb0eb189ee1b9f9b5e6ee1
David Hiller <HillerDavid@users.noreply.github.com>
2018-09-29 19:34:10 -0400

    Buttons now work appropriately/Made code more dry

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,9 @@
 
 $(document).ready(function () {
+    //Starting Array
     var topics = ["labrador", "pug", "dalmation"]
-
+    
+    //
     function loadButtons() {
 
         for (var i = 0; i < topics.length; i++) {
@@ -13,12 +15,20 @@ $(document).ready(function () {
     }
     loadButtons()
 
-    $("#add-gif-button").click(function () {
-        var newButton = $("<button>").text($("user-input").val())
-        $("#gif-button-section").append(newButton)
+    $("#add-gif-button").click(function (event) {
+        event.preventDefault()
+        userInput = $("#user-input").val().trim()
+        if ($.inArray(userInput, topics) === -1) {
+            topics.push(userInput)
+            $("#gif-button-section").empty()
+            loadButtons()
+        } else {
+            alert("Button already exists for " + userInput)
+        }
+        $("#user-input").val("")
     })
 
-    $(".gif-button").click(function () {
+    $("body").on("click", ".gif-button", function () {
         var searchItem = $(this).attr("data-query")
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchItem + "&api_key=Wir4pR8vBsdXyktmyqIESfIqQZ9SB1aq&limit=10"
 
@@ -29,9 +39,9 @@ $(document).ready(function () {
             console.log(response)
             var results = response.data
             for (var i = 0; i < results.length; i++) {
-                var displayDiv = $("<div>").addClass("gif")
+                var displayDiv = $("<div>")
                 var rating = $("<p>").text(results[i].rating)
-                var gifImage = $("<img>").attr("src", results[i].images.fixed_height_still.url).attr("data-still", results[i].images.fixed_height_still.url).attr("data-animate", results[i].images.fixed_height.url).attr("data-state", "still")
+                var gifImage = $("<img>").attr("src", results[i].images.fixed_height_still.url).attr("data-still", results[i].images.fixed_height_still.url).attr("data-animate", results[i].images.fixed_height.url).attr("data-state", "still").addClass("gif")
                 displayDiv.append(rating, gifImage)
                 $("#gif-display").prepend(displayDiv)
             }
@@ -39,15 +49,15 @@ $(document).ready(function () {
 
     })
 
-    $(".gif").click(function () {
+    $("body").on("click", ".gif", function () {
         var state = $(this).attr("data-state")
 
-        if (state === "still"){
+        if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"))
             $(this).attr("data-state", "animate")
         } else {
             $(this).attr("src", $(this).attr("data-still"))
-            $(this.attr("data-state", "still"))
+            $(this).attr("data-state", "still")
         }
     })
 })
